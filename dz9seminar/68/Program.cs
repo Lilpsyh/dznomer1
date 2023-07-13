ff9df159aeba6de306a67db3f79d9b482df91e4f
aleksander savin <savinsaha6@gmail.com>
2023-07-14 03:54:04 +0500

    ура

--- dz9seminar/68/Program.cs
+++ dz9seminar/68/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Введите M");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+int n=Convert.ToInt32(Console.ReadLine());
+int vivod(int m,int n){
+    if(m==0){
+        return n+1;
+    }
+    if((m>0) && (n==0)){
+        return vivod(m-1,1);
+    }
+    if(( m > 0) && (n > 0)){
+        return vivod(m-1,vivod(m,n-1));
+    }
+    else{ return 0;}
+}
+int a=vivod(m,n);
+Console.WriteLine(a);
